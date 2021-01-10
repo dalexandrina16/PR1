a5a23a91a34871a154ad1688bc56c8d0bcfd7ccf
dalexandrina <dina.bizgu.99@gmail.com>
2021-01-10 16:01:36 +0200

    Added the links reading and printing functionality.

--- src/com/utm/entity/ThreadEntity.java
+++ src/com/utm/entity/ThreadEntity.java
@@ -1,5 +1,8 @@
 package com.utm.entity;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
 import com.utm.service.DataAccessService;
 
 import java.io.BufferedReader;
@@ -9,6 +12,10 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class ThreadEntity extends Thread {
 
@@ -28,6 +35,11 @@ public class ThreadEntity extends Thread {
             httpURLConnection.setRequestProperty("X-Access-Token", new DataAccessService().getToken());
             httpURLConnection.setConnectTimeout(5000);
             httpURLConnection.connect();
+            List<String> links = new ArrayList<>();
+            String url = "http://localhost:5000/";
+            String obtained = getFromConnection(httpURLConnection);
+            links.addAll(getAllLinks(obtained, url));
+            System.out.println(links); // printing the obtained links
             StringBuilder content = new StringBuilder();
             BufferedReader in = new BufferedReader(
                     new InputStreamReader(httpURLConnection.getInputStream()));
@@ -46,4 +58,32 @@ public class ThreadEntity extends Thread {
             e.printStackTrace();
         }
     }
+
+    public String getFromConnection(HttpURLConnection httpURLConnection) throws IOException {
+        StringBuilder obtainedBuilder = new StringBuilder();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(httpURLConnection.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null){
+            obtainedBuilder.append(inputLine);
+        }
+        in.close();
+        return obtainedBuilder.toString();
+    }
+
+    public ArrayList<String> getAllLinks(String obtained, String url) {
+        JsonParser jsonParser = new JsonParser();
+        JsonElement jsonElement = jsonParser.parse(obtained).getAsJsonObject().get("link");
+        ArrayList<String> links = new ArrayList<>();
+        Gson gson = new Gson();
+        String json;
+        json = jsonElement.toString();
+        Map<String, Object> map = new HashMap<>();
+        map = (Map<String, Object>)gson.fromJson(json, map.getClass());
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            links.add(url + entry.getValue());
+        }
+        return links;
+    }
+
 }
